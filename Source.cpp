cf4b5a3b72a8a5b5f5f85fc729cd1f33fc03e4cb
rkhasano <rkhasano@online.jccc.edu>
2017-02-02 20:14:38 -0600

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+// Declare function here
+float AddTax(float dollar) {
+
+	float taxAmount;
+	taxAmount = dollar*0.12;
+
+	return dollar + taxAmount;
+
+
+
+}
+
+int main()
+{
+	cout << "Price: $" << 9.99 << " \t with tax: $" << AddTax(9.99) << endl;
+	cout << "Price: $" << 19.95 << " \t with tax: $" << AddTax(19.95) << endl;
+	cout << "Price: $" << 10.00 << " \t with tax: $" << AddTax(10.00) << endl;
+
+	while (true);
+	return 0;
+}
